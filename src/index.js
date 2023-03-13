73c25e05f183dc79d02d9bb86c2164f96d3672c7
Yoon Ji Hoon <hunmonhun@gmail.com>
2023-03-13 16:59:31 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  console.log(1);
-  await next();
-  console.log('hi~ ha');
-
-});
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
 app.use(ctx => {
   ctx.body = 'Hello Koa';
 });
