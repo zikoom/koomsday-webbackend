4a3e73da61191e8def81c25b44fe96b243bac45d
Yoon Ji Hoon <hunmonhun@gmail.com>
2023-03-14 15:41:57 +0900

    koa 라우터 테스트

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('@koa/router')
+const bodyParser = require('koa-bodyparser')
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+
+const app = new Koa();
+const router = new Router();
+const cors = require('@koa/cors')
+
+app.use(cors());
+
+const tempData = {
+  users: ['김김김', '님님님', '딤딤딤'],
+  points: ['10','123123', '551']
+};
+
+router.get('/api/users', ctx => {
+  ctx.body = {ok: true, users: tempData.users}
+})
+
+app.use(bodyParser()).use(router.routes());
+
 
 app.listen(3000, () => {
   console.log('herum server is listening to port 3000');
