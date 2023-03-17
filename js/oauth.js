2047287648400eb69fa0e48c5df86192a7b5a7f7
Yoon Ji Hoon <hunmonhun@gmail.com>
2023-03-17 17:42:49 +0900

    google login을 적용해보자

--- js/oauth.js
+++ js/oauth.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const {google} = require('googleapis');
 
 var v = null;
 
@@ -8,8 +9,20 @@ console.log('oauthJsonPath: ',oauthJsonPath);
 console.log('process.cwd(): ', process.cwd())
 
 const {installed} = JSON.parse(fs.readFileSync(oauthJsonPath))
+const {client_id, client_secret, redirect_uris} = installed;
 
 console.log('secret: ', installed);
+
+const oauth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris
+);
+
+const scopes = [
+  'https://www.googleapis.com/auth/userinfo.profile'
+];
+
 
 
 exports.v = v;
